c9afa648bc4cb35ced685ddf9d3193b012b30737
Hannah Laman <156121079+hanlaman@users.noreply.github.com>
2024-04-11 11:32:56 -0400

    Update test_course_grader.py
    Modifying test cases

--- tests/test_course_grader.py
+++ tests/test_course_grader.py
@@ -1,42 +1,25 @@
 import pytest
 from course_grader import convert_to_letter_grade
-
-class TestConvertToLetterGrade:
 # TODO-1: Add test_exact_grade_boundaries() function
-  def test_exact_boundaries():
-    Test exact boundaries for each letter grade
-    assert convert_to_letter_grade(0) == "F"
-    assert convert_to_letter_grade(59) == "F"
-    assert convert_to_letter_grade(60) == "D"
-    assert convert_to_letter_grade(69) == "D"
-    assert convert_to_letter_grade(70) == "C"
-    assert convert_to_letter_grade(79) == "C"
-    assert convert_to_letter_grade(80) == "B"
-    assert convert_to_letter_grade(89) == "B"
-    assert convert_to_letter_grade(90) == "A"
-    assert convert_to_letter_grade(100) == "A"
-  
-# # TODO-2: Add test_invalid_numerical_score() function
-# def test_invalid_numerical_score(self):
-#   # test ValueError for scores outside the range
-#   with pytest.raises(ValueError) as exc_info:
-#     convert_to_letter_grade(-1)
-#   assert str(exc_info.value) == "Score must be between 0 and 100."
-#   with pytest.raises(ValueError) as exc_info:
-#     convert_to_letter_grade(101)
-#   assert str(exc_info.value) == "Score must be between 0 and 100."
-
-# # TODO-3: Add test_non_numeric_input() function
-# def test_non_numeric_type(self):
-# #test TypeError for non numeric input types
-# with pytest.raises(TypeError) as exc_info:
-#   convert_to_letter_grade("abc")
-# assert str(exc_info.value) == "Score must be a numeric value."
-# with pytest.raises(TypeError) as exc_info:
-#   convert_to_letter_grade([1,2,3])
-# assert str(exc_info.value) == "Score must be a numeric value."
-# with pytest.raises(TypeError) as exc_info:
-#   convert_to_letter_grade(None)
-# assert str(exc_info.value) == "Score must be a numeric value."
-
+def test_exact_boundaries():
+  assert convert_to_letter_grade(59) == 'F', "Test Failed: a score of 59 is an F"
+  assert convert_to_letter_grade(60) == 'D', "Test Failed: a score of 60 is a D"
+  assert convert_to_letter_grade(69) == 'D', "Test Failed: a score of 69 is a D"
+  assert convert_to_letter_grade(70) == 'C', "Test Failed: a score of 70 is a C"
+  assert convert_to_letter_grade(79) == 'C', "Test Failed: a score of 79 is a C"
+  assert convert_to_letter_grade(80) == 'B', "Test Failed: a score of 80 is a B"
+  assert convert_to_letter_grade(89) == 'B', "Test Failed: a score of 89 is a B"
+  assert convert_to_letter_grade(90) == 'A', "Test Failed: a score of 90 is an A"
+  assert convert_to_letter_grade(100) == 'A', "Test Failed: a score of 100 is an A"
+  assert convert_to_letter_grade(0) == 'F', "Test Failed: a score of 0 is an F"
+# TODO-2: Add test_invalid_numerical_score() function
+def test_invalid_numerical_score():
+  with pytest.raises(TypeError):
+    convert_to_letter_grade(-1)
+  with pytest.raises(TypeError):
+    convert_to_letter_grade(101)
+# TODO-3: Add test_non_numeric_input() function
+def test_non_numeric_input():
+  with pytest.raises(TypeError):
+    convert_to_letter_grade("a", 61)
 
