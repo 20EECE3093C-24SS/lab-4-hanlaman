8b9211ee7d4f25f3cc2619fa18de683f111bc442
Hannah Laman <156121079+hanlaman@users.noreply.github.com>
2024-04-11 11:39:55 -0400

    Update test_course_grader.py

--- tests/test_course_grader.py
+++ tests/test_course_grader.py
@@ -2,16 +2,16 @@ import pytest
 from course_grader import convert_to_letter_grade
 # TODO-1: Add test_exact_grade_boundaries() function
 def test_exact_boundaries():
-  assert convert_to_letter_grade(59) == 'F', "Test Failed: a score of 59 is an F"
-  assert convert_to_letter_grade(60) == 'D', "Test Failed: a score of 60 is a D"
-  assert convert_to_letter_grade(69) == 'D', "Test Failed: a score of 69 is a D"
-  assert convert_to_letter_grade(70) == 'C', "Test Failed: a score of 70 is a C"
-  assert convert_to_letter_grade(79) == 'C', "Test Failed: a score of 79 is a C"
-  assert convert_to_letter_grade(80) == 'B', "Test Failed: a score of 80 is a B"
-  assert convert_to_letter_grade(89) == 'B', "Test Failed: a score of 89 is a B"
-  assert convert_to_letter_grade(90) == 'A', "Test Failed: a score of 90 is an A"
-  assert convert_to_letter_grade(100) == 'A', "Test Failed: a score of 100 is an A"
-  assert convert_to_letter_grade(0) == 'F', "Test Failed: a score of 0 is an F"
+  assert convert_to_letter_grade(59) == 'F'
+  assert convert_to_letter_grade(60) == 'D'
+  assert convert_to_letter_grade(69) == 'D'"
+  assert convert_to_letter_grade(70) == 'C'
+  assert convert_to_letter_grade(79) == 'C'
+  assert convert_to_letter_grade(80) == 'B'
+  assert convert_to_letter_grade(89) == 'B'
+  assert convert_to_letter_grade(90) == 'A'
+  assert convert_to_letter_grade(100) == 'A'
+  assert convert_to_letter_grade(0) == 'F'
 # TODO-2: Add test_invalid_numerical_score() function
 def test_invalid_numerical_score():
   with pytest.raises(TypeError):
@@ -22,4 +22,7 @@ def test_invalid_numerical_score():
 def test_non_numeric_input():
   with pytest.raises(TypeError):
     convert_to_letter_grade("a", 61)
-
+  with pytest.raises(TypeError):
+    convert_to_letter_grade([a,b,c])
+  with pytest.raises(TypeError):
+    convert_to_letter_grade(None)
